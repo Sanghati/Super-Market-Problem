193a4cffd0d721339c4f95fbc1b1721722a03e4b
Sanghati Mukherjee <sanghatibm88@gmail.com>
2014-04-29 19:07:06 +0530

    Update list_item.rb

--- list_item.rb
+++ list_item.rb
@@ -16,20 +16,35 @@ class List
 
 	def calculate_total_item(checkout_item)
 		total_price = 0
+		count = 0
 		numb_occurrence = Hash.new(0) 
 		checkout_item.each do |chk_item|
 			numb_occurrence[chk_item] += 1
 		end
 		@items.each do |item|
 			numb_occurrence.each do |chk_item, value|
+
 				if item.item_name.chomp == chk_item
 					if item.offer == true
-						if item.numb_item_offer  == value
+						if item.numb_item_offer  == value 
 							total_price += item.special_price
-						elsif item.numb_item_offer > value ||  item.numb_item_offer < value
+						elsif item.numb_item_offer > value 
 							value.times do
 								total_price += item.unit_price
 							end
+						elsif item.numb_item_offer < value
+							puts value.modulo(item.numb_item_offer).zero?
+							until value.modulo(item.numb_item_offer).zero?
+							 	value -=1
+							 	count +=1
+							end 
+								numb_times = value / item.numb_item_offer.to_i
+								numb_times.times do
+									total_price += item.special_price
+								end
+								count.times do
+									total_price += item.special_price
+								end				 
 						end
 					else
 						total_price += item.unit_price
